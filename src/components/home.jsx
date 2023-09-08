aa0da4579ff1d00acbede8e6aaf27e0dd8080798
Shriyash <mandavekarshriyash@gmail.com>
2023-09-09 02:21:03 +0530

    Start chatbox

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,6 +6,7 @@ import Footer from './footer'
 import './CSS/home.css'
 import Footer1 from './footer1'
 import Update from './updates'
+import ChatboxLogo from './Chatbox/chatboxLogo'
 const Home = () => {
   return (
     <section className='Home'>
@@ -17,6 +18,7 @@ const Home = () => {
       </section>
       <Update/>
       <About />
+      <ChatboxLogo/>
       <Footer1 />
       <Footer />
     </section>
