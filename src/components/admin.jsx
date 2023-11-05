04efdd4d51b5365a8cbead3d03eb9981befb6a9d
Shriyash <mandavekarshriyash@gmail.com>
2023-11-06 02:30:51 +0530

    Add header

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import Header from './header'
 
 class Admin extends React.Component {
   componentDidMount() {
-    // Initialize EmailJS here
-    emailjs.init("X58CaIxbK2xNIb2yR"); // Replace with your User ID
+    emailjs.init("X58CaIxbK2xNIb2yR"); 
   }
 
   sendMailToRecipients = (recipientEmails, currentIndex) => {
-    const serviceID = "service_r72ofma"; // Replace with your service ID
-    const templateID = "template_21lqvvd"; // Replace with your template ID
+    const serviceID = "service_r72ofma";
+    const templateID = "template_21lqvvd";
 
     if (currentIndex >= recipientEmails.length) {
       console.log("All emails sent successfully");
@@ -26,14 +26,12 @@ class Admin extends React.Component {
     emailjs.send(serviceID, templateID, params)
       .then((res) => {
         console.log(`Email sent successfully to ${email}`, res);
-        // Send the next email after a delay (e.g., 2 seconds)
         setTimeout(() => {
           this.sendMailToRecipients(recipientEmails, currentIndex + 1);
-        }, 200);
+        }, 100);
       })
       .catch((err) => {
         console.error(`Error sending email to ${email}`, err);
-        // Handle the error and move on to the next email
         this.sendMailToRecipients(recipientEmails, currentIndex + 1);
       });
   }
@@ -41,12 +39,10 @@ class Admin extends React.Component {
   sendMail = () => {
     const recipientEmails = [
       "mandavekarshriyash@gmail.com",
-      "crtdh@iitgn.ac.in",
       "appollo3579@gmail.com",
       "shriyashmandavekar1234@gmail.com",
       "mandavekar.shriyash@iitgn.ac.in",
       "maprcdata@gmail.com",
-      "vraj.shah@iitgn.ac.in",
       "samparc77@gmail.com"
     ];
 
@@ -55,22 +51,25 @@ class Admin extends React.Component {
 
   render() {
     return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <img src={require('./Assests/Images/admin-page.png')} alt="Admin Page" />
-        <button
-          className="btn btn-primary"
-          onClick={this.sendMail}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            position: "absolute",
-            marginTop: "530px",
-            fontSize: "2rem",
-            fontWeight: "600"
-          }}
-        >
-          Inauguration of Network CRTDH platform
-        </button>
+      <div className='Admin'>
+        <Header/>
+        <div style={{ display: "flex", justifyContent: "center",marginTop:"100px",marginBottom:"100px" }}>
+          <img src={require('./Assests/Images/admin-page.png')} alt="Admin Page" />
+          <button
+            className="btn btn-primary"
+            onClick={this.sendMail}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              position: "absolute",
+              marginTop: "530px",
+              fontSize: "2rem",
+              fontWeight: "600"
+            }}
+          >
+            Inauguration of Network CRTDH Platform
+          </button>
+        </div>
       </div>
     );
   }
