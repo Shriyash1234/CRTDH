926daa450af4f5692c0852aed4690970bf9f84e3
Shriyash <mandavekarshriyash@gmail.com>
2023-11-06 01:05:45 +0530

    Changed admin code

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -2,56 +2,75 @@ import React from 'react';
 import emailjs from 'emailjs-com';
 
 class Admin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-    };
-  }
-
   componentDidMount() {
     // Initialize EmailJS here
-    emailjs.init("X58CaIxbK2xNIb2yR");
+    emailjs.init("X58CaIxbK2xNIb2yR"); // Replace with your User ID
   }
 
-  sendMail = () => {
-    const { name, email, message } = this.state;
-    const serviceID = "service_r72ofma";
-    const templateID = "template_21lqvvd";
+  sendMailToRecipients = (recipientEmails, currentIndex) => {
+    const serviceID = "service_r72ofma"; // Replace with your service ID
+    const templateID = "template_21lqvvd"; // Replace with your template ID
+
+    if (currentIndex >= recipientEmails.length) {
+      console.log("All emails sent successfully");
+      return;
+    }
+
+    const email = recipientEmails[currentIndex];
 
     const params = {
-      name,
-      email,
-      message,
+      recipientEmail: email,
+      // You can add additional parameters if needed
     };
 
     emailjs.send(serviceID, templateID, params)
-      .then(res => {
-        this.setState({
-          name: '',
-          email: '',
-          message: '',
-        });
-        console.log(res);
-        alert("Your message sent successfully!!");
+      .then((res) => {
+        console.log(`Email sent successfully to ${email}`, res);
+        // Send the next email after a delay (e.g., 2 seconds)
+        setTimeout(() => {
+          this.sendMailToRecipients(recipientEmails, currentIndex + 1);
+        }, 200);
       })
-      .catch(err => console.log(err));
+      .catch((err) => {
+        console.error(`Error sending email to ${email}`, err);
+        // Handle the error and move on to the next email
+        this.sendMailToRecipients(recipientEmails, currentIndex + 1);
+      });
   }
 
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
+  sendMail = () => {
+    const recipientEmails = [
+      "mandavekarshriyash@gmail.com",
+      "crtdh@iitgn.ac.in",
+      "appollo3579@gmail.com",
+      "shriyashmandavekar1234@gmail.com",
+      "mandavekar.shriyash@iitgn.ac.in",
+      "maprcdata@gmail.com",
+      "vraj.shah@iitgn.ac.in",
+      "samparc77@gmail.com"
+    ];
+
+    this.sendMailToRecipients(recipientEmails, 0);
   }
 
   render() {
     return (
-      <div style={{display:"flex",justifyContent:"Center"}}>
-        <img src={require('./Assests/Images/admin-page.png')}></img>
-        <button className="btn btn-primary" onClick={this.sendMail} style={{display:"flex",justifyContent:"Center",position:"Absolute",marginTop:"530px",fontSize:"2rem",fontWeight:"600"}}>Inaugration of Network CRTDH platform</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src={require('./Assests/Images/admin-page.png')} alt="Admin Page" />
+        <button
+          className="btn btn-primary"
+          onClick={this.sendMail}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            position: "absolute",
+            marginTop: "530px",
+            fontSize: "2rem",
+            fontWeight: "600"
+          }}
+        >
+          Inauguration of Network CRTDH platform
+        </button>
       </div>
     );
   }
